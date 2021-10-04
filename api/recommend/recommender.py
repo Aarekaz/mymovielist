9ab553ddf2102a2335d79b1d76dfd6681fcb35bf
Anurag Dhungana <anuragdhungana1@gmail.com>
2021-10-04 19:29:29 +0545

    test 1

--- api/recommend/recommender.py
+++ api/recommend/recommender.py
@@ -1,19 +1,20 @@
 import pickle
 import numpy as np
+import models
+from models import model_to_ckpt, model_to_cls
 
-from recommend.models import model_to_ckpt, model_to_cls
 
 class RecommenderWrapper:
     def __init__(self) -> None:
         self.model_name = None
         self.model = None
-    
+
     def set_model(self, new_model):
         if self.model == None or self.model_name != new_model:
             self.model_name = new_model
             self.model = model_to_cls[new_model]()
             self.model.restore(model_to_ckpt[new_model])
-        
+
     def recommend(self, user_context):
         # user context to user vec
         user_item_ids = [int(i) for i in user_context]
