ddcde307a0276b709e2690c2a76830b5e1fdf756
Anurag Dhungana <anuragdhungana5@gmail.com>
2022-01-29 09:46:23 +0545

    initial google login, Button placement left

---
+++ frontend/src/components/login/Logout.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { GoogleLogout } from "react-google-login";
+
+const clientId =
+  "766782824770-ggibhlciuo3gcc39piu75a5kq92ft9jk.apps.googleusercontent.com";
+
+function Logout() {
+  const onSuccess = () => {
+    console.log("Logout made successfully");
+    alert("Logout made successfully ✌");
+  };
+
+  return (
+    <div>
+      <GoogleLogout
+        clientId={clientId}
+        buttonText="Logout"
+        onLogoutSuccess={onSuccess}
+      ></GoogleLogout>
+    </div>
+  );
+}
+
+export default Logout;
