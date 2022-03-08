5eae2c920e4133537ed17bae21fbc5f5c5fff712
HaxXeD <eziovain@gmail.com>
2022-03-08 10:15:18 +0545

    merged function and images integration

--- backend/functions.py
+++ backend/functions.py
@@ -1,7 +1,9 @@
 import streamlit as st
+from item_recommender import df_f
 
-
+posters=[]
 def movie_bar(n):
+    
     md_parasite = "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_FMjpg_UY720_.jpg"
     md_boyhood = "https://m.media-amazon.com/images/M/MV5BMTYzNDc2MDc0N15BMl5BanBnXkFtZTgwOTcwMDQ5MTE@._V1_FMjpg_UX1000_.jpg"
     md_endgame = "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_FMjpg_UY720_.jpg"
@@ -39,4 +41,4 @@ def movie_bar(n):
 
     for i in range(ncol):
         col = cols[i % wcol]
-        col.image(md_parasite, use_column_width="always")
+        col.image(posters[i], use_column_width="always")
--- backend/item_recommender.py
+++ backend/item_recommender.py
@@ -3,6 +3,7 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import requests
 import seaborn as sns
 import streamlit as st
 from scipy.sparse import csr_matrix
@@ -12,6 +13,7 @@ from functions import *
 
 movies = pd.read_csv("data/movies.csv", encoding="latin-1")
 ratings = pd.read_csv("data/ratings.csv", encoding="latin-1")
+links = pd.read_csv("data/links.csv",header=0,delim_whitespace=False)
 
 final_dataset = ratings.pivot(index="movieId", columns="userId", values="rating")
 final_dataset.fillna(0, inplace=True)
@@ -31,8 +33,14 @@ final_dataset.reset_index(inplace=True)
 knn = NearestNeighbors(metric="cosine", algorithm="brute", n_neighbors=20, n_jobs=-1)
 knn.fit(csr_data)
 
+poster = []
+api_key = "40f84ea0b622bc4257dabf9631a401dc"
+api_version = 3
+api_base_url = f"https://api.themoviedb.org/{api_version}"
+
 
 def get_movie_recommendation(movie_name: str, n):
+    global df_f
     n_movies_to_reccomend = n
     movie_list = movies[movies["title"].str.contains(movie_name)]
     if len(movie_list):
@@ -62,22 +70,56 @@ def get_movie_recommendation(movie_name: str, n):
             )
         df = pd.DataFrame(recommend_frame, index=range(1, n_movies_to_reccomend + 1))
 
-        # (index|columns|records|split|table)
-        js = df.to_json(orient="index")
+        new_df = links.loc[links['movieId'].isin(df['Movie ID'])]
+        df1 = new_df.rename(columns={"movieId":"Movie ID"})
+        df_f = pd.merge(df,df1)
+
+        for movie_id in df_f['tmdbId']:    
+            endpoint_path = f"/movie/{movie_id}"
+            endpoint = f"{api_base_url}{endpoint_path}?api_key={api_key}&language=en-US"
+            data = requests.get(endpoint)
+            data = data.json()
+            poster_path = data['poster_path']
+            full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
+            poster.append(full_path)
+
+        df_f['Poster'] = poster
+
+        #(index|columns|records|split|table)
+        js = df_f.to_json(orient='index')
 
         final_list = list()
-        for row in df.iterrows():
+        for row in df_f.iterrows():
             final_list.append(row[1].to_dict())
-
-        with open("notebooks\json_data\data.json", "w") as f:
+        
+        with open('notebooks\json_data\data.json', 'w') as f:
             f.write(json.dumps(final_list, indent=4))
 
         # with open('json_data/data.json', 'w', encoding='utf-8') as f:
         #     json.dump(js, f, ensure_ascii=False, indent=4)
         print(json.dumps(final_list, indent=4))
         st.write("The ", n, "movies recommended to you similar to", movie_name, "are:")
+        
         movie_bar(n)
-        return st.dataframe(df)
+
+        return st.dataframe(df_f)
+
+        # # (index|columns|records|split|table)
+        # js = df.to_json(orient="index")
+
+        # final_list = list()
+        # for row in df.iterrows():
+        #     final_list.append(row[1].to_dict())
+
+        # with open("notebooks\json_data\data.json", "w") as f:
+        #     f.write(json.dumps(final_list, indent=4))
+
+        # # with open('json_data/data.json', 'w', encoding='utf-8') as f:
+        # #     json.dump(js, f, ensure_ascii=False, indent=4)
+        # print(json.dumps(final_list, indent=4))
+        # st.write("The ", n, "movies recommended to you similar to", movie_name, "are:")
+        # movie_bar(n)
+        # return st.dataframe(df)
 
     else:
         return st.error("Opps! No movies found. Please check your input")
