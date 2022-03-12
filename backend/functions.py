a35232cf9ac8d649bc602ce7c9bf71efc03b3d25
Anurag Dhungana <36888347+Aarekaz@users.noreply.github.com>
2022-03-12 09:23:32 +0545

    Cleanup

--- backend/functions.py
+++ backend/functions.py
@@ -139,38 +139,51 @@ def show_data():
     st.markdown(
         "The datasets below was obtained through TMDB and IDMB. We also used THe movie lens data into our project. The movies available in this dataset are in correspondence with the movies that are listed in the **MovieLens Latest Full Dataset** comprising of 26 million ratings on 45,000 movies from 27,000 users."
     )
-    #     st.write("The features of the dataset are  ")
-    #     st.markdown(
-    #         """ ### Features
+    st.write("The features of the dataset are  ")
+    st.markdown(
+        """ ### Features
 
-    # * **adult:** Indicates if the movie is X-Rated or Adult.
-    # * **belongs_to_collection:** A stringified dictionary that gives information on the movie series the particular film belongs to.
-    # * **budget:** The budget of the movie in dollars.
-    # * **genres:** A stringified list of dictionaries that list out all the genres associated with the movie.
-    # * **homepage:** The Official Homepage of the move.
-    # * **id:** The ID of the move.
-    # * **imdb_id:** The IMDB ID of the movie.
-    # * **original_language:** The language in which the movie was originally shot in.
-    # * **original_title:** The original title of the movie.
-    # * **overview:** A brief blurb of the movie.
-    # * **popularity:** The Popularity Score assigned by TMDB.
-    # * **poster_path:** The URL of the poster image.
-    # * **production_companies:** A stringified list of production companies involved with the making of the movie.
-    # * **production_countries:** A stringified list of countries where the movie was shot/produced in.
-    # * **release_date:** Theatrical Release Date of the movie.
-    # * **revenue:** The total revenue of the movie in dollars.
-    # * **runtime:** The runtime of the movie in minutes.
-    # * **spoken_languages:** A stringified list of spoken languages in the film.
-    # * **status:** The status of the movie (Released, To Be Released, Announced, etc.)
-    # * **tagline:** The tagline of the movie.
-    # * **title:** The Official Title of the movie.
-    # * **video:** Indicates if there is a video present of the movie with TMDB.
-    # * **vote_average:** The average rating of the movie.
-    # * **vote_count:** The number of votes by users, as counted by TMDB. """
-    # )
+    * **adult:** Indicates if the movie is X-Rated or Adult.
+    * **belongs_to_collection:** A stringified dictionary that gives information on the movie series the particular film belongs to.
+    * **budget:** The budget of the movie in dollars.
+    * **genres:** A stringified list of dictionaries that list out all the genres associated with the movie.
+    * **homepage:** The Official Homepage of the move.
+    * **id:** The ID of the move.
+    * **imdb_id:** The IMDB ID of the movie.
+    * **original_language:** The language in which the movie was originally shot in.
+    * **original_title:** The original title of the movie.
+    * **overview:** A brief blurb of the movie.
+    * **popularity:** The Popularity Score assigned by TMDB.
+    * **poster_path:** The URL of the poster image.
+    * **production_companies:** A stringified list of production companies involved with the making of the movie.
+    * **production_countries:** A stringified list of countries where the movie was shot/produced in.
+    * **release_date:** Theatrical Release Date of the movie.
+    * **revenue:** The total revenue of the movie in dollars.
+    * **runtime:** The runtime of the movie in minutes.
+    * **spoken_languages:** A stringified list of spoken languages in the film.
+    * **status:** The status of the movie (Released, To Be Released, Announced, etc.)
+    * **tagline:** The tagline of the movie.
+    * **title:** The Official Title of the movie.
+    * **video:** Indicates if there is a video present of the movie with TMDB.
+    * **vote_average:** The average rating of the movie.
+    * **vote_count:** The number of votes by users, as counted by TMDB. """
+    )
 
     # movies = pd.read_csv("EDA_data/movies_metadata.csv", sep=";")
     # st.write(movies.head())
 
     movies = pd.read_csv("EDA_data/movies_metadata.csv", on_bad_lines="skip")
     st.write(movies.head())
+
+    col1, col2 = st.columns(2)
+
+    rec_movie = pd.read_csv("data/movies.csv", on_bad_lines="skip")
+    rec_rating = pd.read_csv("data/ratings.csv", on_bad_lines="skip")
+
+    with col1:
+        st.markdown(" #### Movies Dataset ")
+        st.write(rec_movie.head())
+
+    with col2:
+        st.maarkdown(" #### Ratings Dataset ")
+        st.write(rec_rating.head())
